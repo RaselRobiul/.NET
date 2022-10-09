ad24938a5221606da5ab91d4f3c307b9a517a169
RaselRobiul <raselrobiul828@gmail.com>
2022-10-09 20:48:35 +0600

    first

---
+++ MT_ASSIGNMENT_1_Validation/Controllers/ValidationController.cs
@@ -0,0 +1,29 @@
+﻿using MT_ASSIGNMENT_1_Validation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MT_ASSIGNMENT_1_Validation.Controllers
+{
+    public class ValidationController : Controller
+    {
+        // GET: Validation
+        [HttpGet]
+        public ActionResult Validationf1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Validationf1(Validation valid)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(valid);
+        }
+    }
+}
